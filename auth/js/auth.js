dde5c1d60725956fcb0d67653691b37c0e51b542
RodriguezCesarr <60372368+RodriguezCesarr@users.noreply.github.com>
2020-02-05 21:04:53 -0500

    authentication page

--- auth/js/auth.js
+++ auth/js/auth.js
@@ -2,8 +2,16 @@ const loginEmail = document.getElementById('login-email');
 const loginPassword = document.getElementById('login-password');
 const loginButton = document.getElementById('login-button');
 const loginMessage = document.getElementById('login-message')
+const userName = document.getElementById('user-name');
 
 loginButton.onclick= function() {
-	console.log(loginEmail.value, loginPassword.value);
-	loginMessage.textContent = "you logged in";
+	fb.login(loginEmail.value, loginPassword.value);
 };
+
+function onError(errorMessae){
+	loginMessage.textContent = errorMessae;
+}
+
+function userLoggedIn(uid, displayName){
+	userName.tectContemt = "welcome" + displayName;
+}
