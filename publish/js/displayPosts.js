90e33ede4ba631bd439ddaf7df683aa4116a69b0
RodriguezCesarr <60372368+RodriguezCesarr@users.noreply.github.com>
2020-03-09 19:50:41 -0400

    1

--- publish/js/displayPosts.js
+++ publish/js/displayPosts.js
@@ -8,17 +8,20 @@ function createPost(postData, userInfo, postId) {
 	const post = js.createEl('div', 'post');
 	
 	// posts in reverse chronological order
-	posts.insertBefore(post, posts.firstElementChild);
+	//no cronological order
+	//posts.insertBefore(post, posts.firstElementChild);
 
 	// add posts in chronological
-	//  posts.appendChild(post);
+	posts.appendChild(post);
 	
 	const text = js.createEl('div', 'post-text', postData.text);
 	post.appendChild(text);
 
 	// post info 
-
+const info = js.createEl('div', 'post-info');
+post.appendChild(info);
 	// author userInfo.displayName
+const 
 	// date js.formatDate(postData.date) 
 	
 }
